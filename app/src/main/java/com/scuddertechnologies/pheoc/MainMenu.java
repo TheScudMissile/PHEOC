577a38ba84f3c85e3e17f00cc6cd559a593908de
bscudder <SSscuds37@gmail.com>
2016-07-03 23:31:11 -0500

    Added "Problem" activity with custom arrow icon for FAB.  This activity contains a single EditText.

--- app/src/main/java/com/scuddertechnologies/pheoc/MainMenu.java
+++ app/src/main/java/com/scuddertechnologies/pheoc/MainMenu.java
@@ -1,7 +1,10 @@
 package com.scuddertechnologies.pheoc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void startNewExperiment(View view) {
+
+        Intent intent = new Intent(this, Problem.class);
+        startActivity(intent);
+    }
+
+    public void viewPastExperiments(View view) {
+
+        Toast.makeText(MainMenu.this, "Frig off Lahey this doesn't work yet!",
+                Toast.LENGTH_SHORT).show();
+
     }
 }
