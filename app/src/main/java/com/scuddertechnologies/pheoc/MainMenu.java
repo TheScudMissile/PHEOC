34ad69a1c7a89616d49422093e1b75eebc3093a1
bscudder <SSscuds37@gmail.com>
2016-07-21 19:48:46 -0500

    deleted some worthless stuff

--- app/src/main/java/com/scuddertechnologies/pheoc/MainMenu.java
+++ app/src/main/java/com/scuddertechnologies/pheoc/MainMenu.java
@@ -17,13 +17,14 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
     }
 
+    //go to Experiment activity
     public void startNewExperiment(View view) {
 
         Intent intent = new Intent(this, Experiment.class);
         startActivity(intent);
     }
 
-
+    //go to Past_Experiments activity
     public void viewPastExperiments(View view) {
 
         Intent intent = new Intent(this, Past_Experiments.class);
